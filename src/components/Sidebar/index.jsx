00116aeef19ad0360df86d606fd41d8bae5c11a6
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-03-13 14:39:12 -0300

    avatar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,18 +1,17 @@
 import { Pencil } from 'phosphor-react';
+import { Avatar } from '../Avatar';
 import styles from './Sidebar.module.css';
 
-export function Sidebar (){
+export function Sidebar() {
     return (
         <aside className={styles.sidebar}>
             <img
-            className={styles.cover} 
-            src="https://images.unsplash.com/photo-1678621359905-f479e9d6258b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50" 
+                className={styles.cover}
+                src="https://images.unsplash.com/photo-1678621359905-f479e9d6258b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=300&q=50"
             />
-            
+
             <div className={styles.profile}>
-                <img
-                className={styles.avatar} 
-                src="https://avatars.githubusercontent.com/u/81534727?v=4"  />
+                <Avatar src="https://avatars.githubusercontent.com/u/81534727?v=4" />
                 <strong>Marcelo Costa</strong>
                 <span>Web developer</span>
             </div>
